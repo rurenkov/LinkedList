0fac043d9d1f7f684101e4a43233cf6e3acd5e2a
Vitalii Rurenko <vitaliiru@nice.com>
2016-12-18 15:55:13 +0200

    d

---
+++ LinkedList/LinkedList/Node.cs
@@ -0,0 +1,24 @@
+﻿using LinkedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+   public  class Node  <T>: LinkedList<T>
+    {
+
+        Node<T> node = new Node<T>();
+
+        public T value;
+        public Node(T value)
+        {
+            this.value = value;
+        }
+
+
+
+    }
+}
